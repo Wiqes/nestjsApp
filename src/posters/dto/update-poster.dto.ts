1bafb2b87788020d6cff46eaa24200d8e0a66065
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-09 18:20:46 +0200

    save

--- src/posters/dto/update-poster.dto.ts
+++ src/posters/dto/update-poster.dto.ts
@@ -1,8 +1,16 @@
+import { IsBoolean } from 'class-validator';
+
 export class UpdatePosterDto {
     readonly title: string;
+
     readonly photo: string;
+
+    @IsBoolean()
     readonly isInShoppingCart: boolean;
+
     readonly sellerName: string;
+
     readonly price: number;
+
     readonly description: string;
 }
