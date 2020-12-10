baf63c10d0d90ddf99c48818c01a64630286da57
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-10 17:00:06 +0200

    save

--- src/posters/shopping-cart/shopping-cart.controller.ts
+++ src/posters/shopping-cart/shopping-cart.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Poster } from 'src/schemas/poster.schema';
 import { ShoppingCartService } from './shopping-cart.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('shopping-cart')
 export class ShoppingCartController {
     constructor(private readonly shoppingCartService: ShoppingCartService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     getAll(): Promise<Poster[]> {
         return this.shoppingCartService.getAll();
