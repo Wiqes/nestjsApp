f16c9e33735289f155565583c18cd5bdc1d42309
KSkryabin <Kirill.Skryabin@envision.design>
2020-12-29 12:45:00 +0200

    save

--- src/posters/all-posters/posters.module.ts
+++ src/posters/all-posters/posters.module.ts
@@ -3,11 +3,13 @@ import { PostersController } from './posters.controller';
 import { PostersService } from './posters.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Poster, PosterSchema } from './schemas/poster.schema';
+import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
+import { ShoppingCartModule } from '../shopping-cart/shopping-cart.module';
 
 @Module({
-    controllers: [PostersController],
+    controllers: [PostersController /*ShoppingCartService*/],
     providers: [PostersService],
-    imports: [MongooseModule.forFeature([{ name: Poster.name, schema: PosterSchema }])],
+    imports: [MongooseModule.forFeature([{ name: Poster.name, schema: PosterSchema }]) /*ShoppingCartModule*/],
     exports: [PostersService, MongooseModule.forFeature([{ name: Poster.name, schema: PosterSchema }])],
 })
 export class PostersModule {}
--- src/posters/all-posters/posters.service.ts
+++ src/posters/all-posters/posters.service.ts
@@ -1,13 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Poster, PosterDocument } from 'src/posters/all-posters/schemas/poster.schema';
 import { CreatePosterDto } from './dto/create-poster.dto';
 import { UpdatePosterDto } from './dto/update-poster.dto';
+import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
 
 @Injectable()
 export class PostersService {
-    constructor(@InjectModel(Poster.name) private posterModel: Model<PosterDocument>) {}
+    constructor(
+        @InjectModel(Poster.name) private posterModel: Model<PosterDocument>, //@Inject(forwardRef(() => ShoppingCartService)) private shoppingCartService: ShoppingCartService,
+    ) {}
 
     private static notFoundException(id: string): NotFoundException {
         return new NotFoundException(`The poster with id: '${id}' has not been found!`);
